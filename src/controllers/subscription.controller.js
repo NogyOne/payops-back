5d2289d40b8cc2522a773649349e1e225139a38f
Germán Gómez <german_gomez1205@hotmail.com>
2024-06-18 13:36:46 -0700

    update subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,101 +1,105 @@
 import { prisma } from '../utils/dbConnect.js'
 
-export const addSubscription = async (monthsPaid) => {
-    try {
-        const startDate = new Date()
-        startDate.setHours(0, 0, 0, 0)
-        const endDate = new Date(startDate)
-        endDate.setMonth(endDate.getMonth() + monthsPaid)
+export const addSubscription = async monthsPaid => {
+  try {
+    const startDate = new Date()
+    startDate.setHours(0, 0, 0, 0)
+    const endDate = new Date(startDate)
+    endDate.setMonth(endDate.getMonth() + monthsPaid)
 
-        const newSubscription = await prisma.subscription.create({
-            data: {
-                startDate: startDate,
-                endDate: endDate,
-                monthsPaid: monthsPaid
-            }
-        })
-        return newSubscription
-        //res.json(newSubscription)
-    } catch (error) {
-        throw new Error(error.message)
-    }
+    const newSubscription = await prisma.subscription.create({
+      data: {
+        startDate: startDate,
+        endDate: endDate,
+        monthsPaid: monthsPaid,
+      },
+    })
+    return newSubscription
+    //res.json(newSubscription)
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 export const getSubscriptions = async (req, res) => {
-    try {
-        const subscriptions = await prisma.subscription.findMany()
-        res.json(subscriptions)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const subscriptions = await prisma.subscription.findMany()
+    res.json(subscriptions)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export const updateSubStatus = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    if(!id){
-        return res.status(401).json({message: 'Please provide a Subscription to update its status.'})
-    }
+  if (!id) {
+    return res
+      .status(401)
+      .json({ message: 'Please provide a Subscription to update its status.' })
+  }
 
-    try {
-        const updatedSubStatus = await prisma.subscription.update({
-            where: {
-                id: +id
-            },
-            data:{
-                status: 'EXPIRED'
-            }
-        })
-        res.json(updatedSubStatus)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const updatedSubStatus = await prisma.subscription.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        status: 'EXPIRED',
+        emailExpiredSent: true,
+      },
+    })
+    res.json(updatedSubStatus)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export const updateSubscription = async (id, monthsPaid) => {
-    try {        
-        const startDate = new Date()
-        startDate.setHours(0, 0, 0, 0)
-        
-        const endDate = new Date(startDate)
-        endDate.setMonth(endDate.getMonth() + parseInt(monthsPaid))
+  try {
+    const startDate = new Date()
+    startDate.setHours(0, 0, 0, 0)
 
-        const updatedSubscription = await prisma.subscription.update({
-            where: {
-                id: +id
-            },
-            data: {
-                startDate,
-                endDate,
-                monthsPaid: +monthsPaid,
-                status: 'CURRENT'
-            }
-        })
-        return updatedSubscription
-        //res.json(updatedSubscription)
-    } catch (error) {
-        throw new Error(error.message)
-    }
+    const endDate = new Date(startDate)
+    endDate.setMonth(endDate.getMonth() + parseInt(monthsPaid))
+
+    const updatedSubscription = await prisma.subscription.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        startDate,
+        endDate,
+        monthsPaid: +monthsPaid,
+        status: 'CURRENT',
+        emailExpiredSent: false,
+      },
+    })
+    return updatedSubscription
+    //res.json(updatedSubscription)
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 export const deleteSubscription = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    if(!id){
-        return res.status(404).json('Please select a customer.')
-    }
+  if (!id) {
+    return res.status(404).json('Please select a customer.')
+  }
 
-    try {
-        const deletedSub = await prisma.subscription.delete({
-            where: {
-                id: +id
-            },
-            include: {
-                customerUser: true
-            }            
-        })
-        res.json(deletedSub)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const deletedSub = await prisma.subscription.delete({
+      where: {
+        id: +id,
+      },
+      include: {
+        customerUser: true,
+      },
+    })
+    res.json(deletedSub)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
